5d910fe41d5408665f5cb40ba48b8d609e350914
Denys <anokrorodile@gmail.com>
2023-09-23 19:37:53 +0300

    ill write change logs later

--- saver.py
+++ saver.py
@@ -1,33 +1,92 @@
-import json
+from bs4 import BeautifulSoup as bs
+import requests
+import argparse
+
+USER_AGENT = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 "
+              "Safari/537.36")
+# US English
+LANGUAGE = "en-US,en;q=0.5"
 
 
-# Function to get user input
-def get_user_input():
-    name = input("Enter your name: ")
-    button1_url = input("Button1 URL:")
-    button2_url = input("Button2 URL:")
-    button1_label = input("Button1 label:")
-    button2_label = input("Button2 label:")
+def scrape_weather_data(url):
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+    html = session.get(url)
+    # create a new soup
+    soup = bs(html.text, "html.parser")
+    # store all results in this dictionary
+    result = {'region': soup.find("div", attrs={"id": "wob_loc"}).text,    # extract region
+              'temp_now': soup.find("span", attrs={"id": "wob_tm"}).text,    # extract temperature now
+              'dayhour': soup.find("div", attrs={"id": "wob_dts"}).text,    # get the day and hour now
+              'weather_now': soup.find("span", attrs={"id": "wob_dc"}).text}    # get the actual weather
+    # get the actual weather
 
-    user_info = {
-        "name": name,
-        "button1_url": button1_url,
-        "button2_url": button2_url,
-        "button1_label": button1_label,
-        "button2_label": button2_label
+    weather_emojis = {
+        "Partly sunny": "🌤️",
+        "Scattered thunderstorms": "⛈️",
+        "Showers": "🌦️",
+        "Scattered showers": "🌧️",
+        "Rain and snow": "🌧️❄️",
+        "Overcast": "☁️",
+        "Light snow": "🌨️",
+        "Freezing drizzle": "🌧️❄️☔",
+        "Chance of rain": "🌦️🌧️",
+        "Sunny": "☀️",
+        "Clear": "🌞",
+        "Clear with periodic clouds": "🌤️",
+        "Mostly sunny": "🌤️",
+        "Partly cloudy": "⛅",
+        "Mostly cloudy": "🌥️",
+        "Chance of storm": "⛈️",
+        "Rain": "🌧️",
+        "Chance of snow": "❄️🌨️",
+        "Cloudy": "☁️",
+        "Mist": "🌫️",
+        "Storm": "⛈️🌩️",
+        "Thunderstorm": "🌩️⛈️",
+        "Chance of tstorm": "🌦️⛈️",
+        "Sleet": "🌧️❄️",
+        "Snow": "❄️",
+        "Icy": "❄️🌬️",
+        "Dust": "🌫️",
+        "Fog": "🌫️",
+        "Smoke": "🌫️",
+        "Haze": "🌫️",
+        "Flurries": "❄️🌨️",
+        "Light rain": "🌧️",
+        "Snow showers": "🌨️🌦️",
+        "Hail": "🌨️⛈️",
     }
 
-    return user_info
+    result['emoji'] = weather_emojis.get(result['weather_now'], "❓")
+    return result
 
 
-# Function to save user input to a JSON file
-def save_to_json(user_info, filename):
-    with open(filename, 'w') as json_file:
-        json.dump(user_info, json_file, indent=4)
+def get_weather(region):
+    URL = "https://www.google.com/search?lr=lang_en&ie=UTF-8&q=weather"
+    if region:
+        region = region.replace(" ", "+")
+        language = '&hl=en'
+        URL += f"+{region}"
+        URL += f"{language}"
+    # get data
+    data = scrape_weather_data(URL)
+    return data
 
 
-if __name__ == "__main__":
-    user_info = get_user_input()
-    filename = "save.json"
-    save_to_json(user_info, filename)
-    print(f"User information has been saved to {filename}")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Quick Script for Extracting Weather data using Google Weather")
+    parser.add_argument("region", nargs="?",
+                        help="""Region to get weather for. Default is your current location determined by your IP 
+                        Address.""",
+                        default="")
+
+    args = parser.parse_args()
+    weather_data = get_weather(args.region)
+
+    # Print the weather data
+    print("Weather Data:")
+    for key, value in weather_data.items():
+        print(f"{key}: {value}")
