96814275731ad0d52bf16488ba3f416f03e6a305
Denys <anokrorodile@gmail.com>
2023-09-23 19:34:41 +0300

    ill write change logs later

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time_getter
 def discord_rich_presence():
     # Replace 'desired_region' with the region you want weather data for
     desired_region = "Tokyo"  # Replace with your desired region
-    weather_data = weather_parcer.get_weather(desired_region)
+    weather_data = weather_parser.get_weather(desired_region)
     # Access the weather values
     client_id = '1154071316425613312'  # Fake ID, put your real one here
     RPC = Presence(client_id, pipe=0)  # Initialize the client class
@@ -23,7 +23,7 @@ def discord_rich_presence():
             large_image=f"{reader.name}",
             large_text=f"asd",
             details="RAM: " + str(mem_per) + "%" "   CPU: " + str(cpu_per) + "%",
-            state=f"{weather_data['temp_now']}°{weather_data['emoji']}  {time_getter.get_current_time()}",
+            state=f"{weather_data['temp_now']}°{weather_data['emoji']}  {time_getter.current_time}",
             buttons=[
                 {"label": reader.button1_label, "url": reader.button1_url},
                 {"label": reader.button1_label, "url": reader.button2_url}
---
+++ time_getter.py
@@ -0,0 +1,6 @@
+from datetime import datetime as dt
+
+current_time = dt.now()
+current_time = current_time.strftime("%A %H:%M")
+
+# add custom timezones and rework this trash
